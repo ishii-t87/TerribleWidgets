074445c5c6541c313d5b2827bd94b42ccdb4e9de
i_t <>
2018-11-07 18:04:54 +0900

    日付定義

---
+++ android/app/src/main/java/itproj/personal/com/terriblewidgets/DateDefine.kt
@@ -0,0 +1,14 @@
+package itproj.personal.com.terriblewidgets
+
+/**
+ *
+ * @brief 日付を定義する構造体
+ *
+ * */
+class DateDefine(year: Int = 1970, month: Int = 1, date: Int = 1, hourOfDay: Int = 0, minute: Int = 0) {
+    val YEAR: Int = year
+    val MONTH: Int = month
+    val DATE: Int = date
+    val HOUR_OF_DAY: Int = hourOfDay
+    val MINUTE: Int = minute
+}
